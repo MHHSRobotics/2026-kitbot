5e614a3fe14c8e62f91e34f710698c386fa3a512
npasetto0 <blasphemorgulus@gmail.com>
2026-01-16 13:42:51 -0800

    Build and move to 2026.2.1

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026-kitbot";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 6;
+    public static final String GIT_SHA = "a38bef271ea6c779c4ba45afa63fbdba5164ce22";
+    public static final String GIT_DATE = "2026-01-15 19:08:03 PST";
+    public static final String GIT_BRANCH = "npasetto";
+    public static final String BUILD_DATE = "2026-01-15 19:42:28 PST";
+    public static final long BUILD_UNIX_TIME = 1768534948978L;
+    public static final int DIRTY = 0;
+
+    private BuildConstants() {}
+}
--- src/main/java/frc/robot/commands/LaunchSequence.java
+++ src/main/java/frc/robot/commands/LaunchSequence.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import frc.robot.Constants.FuelConstants;
 import frc.robot.subsystems.CANFuelSubsystem;
 
@@ -12,12 +13,10 @@ import frc.robot.subsystems.CANFuelSubsystem;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class LaunchSequence extends SequentialCommandGroup {
-  /** Creates a new LaunchSequence. */
-  public LaunchSequence(CANFuelSubsystem fuelSubsystem) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-        new SpinUp(fuelSubsystem).withTimeout(FuelConstants.SPIN_UP_SECONDS),
-        new Launch(fuelSubsystem));
-  }
+    /** Creates a new LaunchSequence. */
+    public LaunchSequence(CANFuelSubsystem fuelSubsystem) {
+        // Add your commands in the addCommands() call, e.g.
+        // addCommands(new FooCommand(), new BarCommand());
+        addCommands(new SpinUp(fuelSubsystem).withTimeout(FuelConstants.SPIN_UP_SECONDS), new Launch(fuelSubsystem));
+    }
 }
