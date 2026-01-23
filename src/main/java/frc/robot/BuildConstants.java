5dfc060a5fc1350c3d604fc03151cf588f2a184c
Maxwell-Delatorre <maxwelldelatorre2009@gmail.com>
2026-01-22 17:53:56 -0800

    Auto Added!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026-kitbot";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 7;
-    public static final String GIT_SHA = "5e614a3fe14c8e62f91e34f710698c386fa3a512";
-    public static final String GIT_DATE = "2026-01-16 13:42:51 PST";
-    public static final String GIT_BRANCH = "npasetto";
-    public static final String BUILD_DATE = "2026-01-21 15:44:10 PST";
-    public static final long BUILD_UNIX_TIME = 1769039050771L;
+    public static final int GIT_REVISION = 11;
+    public static final String GIT_SHA = "fcd451aba3dc559bdd8f5763e724a39d79e38747";
+    public static final String GIT_DATE = "2026-01-22 16:53:03 PST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-01-22 17:52:14 PST";
+    public static final long BUILD_UNIX_TIME = 1769133134569L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
