71726e23434f20b3bed27a6bbc593728da2d0fad
npasetto0 <blasphemorgulus@gmail.com>
2026-02-03 17:37:44 -0800

    Fixed code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package frc.robot;
 
 /**
@@ -8,34 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026-kitbot";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 11;
-    public static final String GIT_SHA = "872ce235fabf7d34bb033408fbb5953472bf8f61";
-    public static final String GIT_DATE = "2026-01-24 14:29:12 PST";
+    public static final int GIT_REVISION = 12;
+    public static final String GIT_SHA = "8e23eba4c880bcb3f5b94e214f582ec4ee1f4f91";
+    public static final String GIT_DATE = "2026-02-03 17:21:22 PST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-02-03 17:11:34 PST";
-    public static final long BUILD_UNIX_TIME = 1770167494437L;
+    public static final String BUILD_DATE = "2026-02-03 17:22:58 PST";
+    public static final long BUILD_UNIX_TIME = 1770168178628L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
-=======
-package frc.robot;
-
-/**
- * Automatically generated file containing build version information.
- */
-public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "2026-kitbot";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 11;
-    public static final String GIT_SHA = "fcd451aba3dc559bdd8f5763e724a39d79e38747";
-    public static final String GIT_DATE = "2026-01-22 16:53:03 PST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-22 17:52:14 PST";
-    public static final long BUILD_UNIX_TIME = 1769133134569L;
-    public static final int DIRTY = 1;
-
-    private BuildConstants() {}
-}
->>>>>>> 5dfc060a5fc1350c3d604fc03151cf588f2a184c
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
@@ -49,55 +48,3 @@ public final class Constants {
     public static final boolean driveEnabled = false;
     public static final boolean fuelEnabled = true;
 }
-=======
-package frc.robot;
-
-import edu.wpi.first.wpilibj.RobotBase;
-
-import com.ctre.phoenix6.CANBus;
-
-public final class Constants {
-
-    // Enum for different run modes for the code
-    public static enum Mode {
-        /** Running on a real robot. */
-        REAL,
-
-        /** Running a physics simulator. */
-        SIM,
-
-        /** Replaying from a log file. */
-        REPLAY,
-
-        // WIP physics simulator
-        PHYSICS_SIM,
-    }
-
-    public static final Mode simMode =
-            Mode.SIM; // Default simulation mode, should be SIM for regular simulation and REPLAY for replays.
-
-    public static final Mode currentMode =
-            RobotBase.isReal() ? Mode.REAL : simMode; // Current mode the robot program is in
-
-    public static final double loopTime = 0.02; // Period of main robot loop, 20ms default
-
-    public static final CANBus defaultBus = new CANBus("rio"); // CAN bus used for non-drive motors
-
-    public static final CANBus driveBus = new CANBus("rio"); // CAN bus used for drive motors (rio for kitbot)
-
-    public static final double loopOverrunWarningTimeout =
-            0.2; // Amount of time a robot tick can take before reporting a warning to DS
-
-    public static final double brownoutVoltage = 6.0; // Voltage at which brownout protection occurs
-
-    public static final double lowBatteryVoltage = 11.8; // Voltage at which low battery warning appears
-
-    public static final double lowBatteryTime = 0.5; // How long to wait before reporting low battery
-
-    public static final boolean simIsRedAlliance = false; // Whether simulated FMS is on red alliance
-
-    // Toggles for subsystems
-    public static final boolean driveEnabled = true;
-    public static final boolean fuelEnabled = true;
-}
->>>>>>> 5dfc060a5fc1350c3d604fc03151cf588f2a184c
